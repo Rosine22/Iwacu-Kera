a1be385a27f90c4075aca80e6e05fdb49906fb8b
Be9321 <belyseurwidukunda@gmail.com>
2024-05-24 08:38:24 +0200

    chabges

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -4,8 +4,9 @@ const User = require('../models/adminModel');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+
 const register = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password,role } = req.body;
   try {
     const hashedPassword = await bcryptjs.hash(password, 10);
     const user = new User({ username, password: hashedPassword });
@@ -16,25 +17,37 @@ const register = async (req, res) => {
   }
 };
 
+
 const login = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, role } = req.body;
+
   try {
+    // Attempt to find the user by username
     const user = await User.findOne({ username });
+
+    // If no user is found, respond with an error
     if (!user) {
-      return res.status(400).json({ message: 'Invalid username or password' });
+      return res.status(400).json({ message: 'Invalid credentials' });
     }
 
+    // Verify the password against the stored hashed password
     const isMatch = await bcryptjs.compare(password, user.password);
+
+    // If the passwords do not match, respond with an error
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid username or password' });
     }
 
-    const token = jwt.sign({ id: user._id, role: user.role }, 'your_secret_key', { expiresIn: '1h' });
-    res.json({ token });
+    // If the user is found and the password matches, generate a JWT token
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    // Respond with the token
+    res.json({ token: token, message: 'Login successful' });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: error.message });
+    console.error(error); // Log the error for debugging purposes
+    res.status(500).json({ message: 'Internal server error' }); // Send a generic error message to the client
   }
 };
+
 
 module.exports = { register, login };
